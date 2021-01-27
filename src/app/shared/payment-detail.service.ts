f88fbcdbb38bec9bb67e34b6a58ba657321af2cf
AlcatrazALT <olegmazalevsky21@yandex.ru>
2021-01-27 15:51:25 +0700

    feat: Show list of data

--- src/app/shared/payment-detail.service.ts
+++ src/app/shared/payment-detail.service.ts
@@ -9,10 +9,15 @@ export class PaymentDetailService {
 
   constructor(private http: HttpClient) { }
 
-  formData:PaymentDetail = new PaymentDetail();
+  formData:PaymentDetail = new PaymentDetail()
   readonly baseUrl = 'http://localhost:54848/api/PaymentDetail'
+  list: PaymentDetail[]
 
   postPaymentDetail(){
     return this.http.post(this.baseUrl, this.formData)
+  }
+
+  refreshList(){
+    this.http.get(this.baseUrl).toPromise().then(res => this.list = res as PaymentDetail[])
   }
 }
